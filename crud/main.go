925e71347088b7a916752c85830a9f25ba0e6d9e
Saurabh <risabht043@gmail.com>
2026-02-15 16:58:02 +0530

    update method done

--- crud/main.go
+++ crud/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	// "io/ioutil"
 	"net/http"
@@ -12,10 +14,10 @@ type TODO struct {
 	UserId    int    `json:"userId"`
 	Id        int    `json:"id"`
 	Title     string `json:"title"`
-	Completed bool    `json:"completed"`
+	Completed bool   `json:"completed"`
 }
 
-func main() {
+func readFunction() {
 	fmt.Println("learing crud")
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
@@ -42,4 +44,92 @@ func main() {
 		return
 	}
 	fmt.Println("ToDo", todo)
+}
+
+func createFunction() {
+	todo := TODO{
+		UserId:    23,
+		Title:     "Saurabh",
+		Completed: false,
+	}
+
+	//consvert todo i json using marshall
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error happening", err)
+		return
+	}
+
+	//convert data in string
+	jsonstring := string(jsonData)
+
+	//convert json data in to reader
+	jsonReader := strings.NewReader(jsonstring)
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+	//http post take three things:- url, type of content, reader type data
+
+	//send post request
+	res, err := http.Post(myUrl, "application/json", jsonReader)
+
+	if err != nil {
+		fmt.Println("error happnes", err)
+	}
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println("Response", string(data))
+}
+
+func updateFunction() {
+	todo := TODO{
+		UserId:    23,
+		Title:     "Saurabh Tiwari",
+		Completed: false,
+	}
+
+	//consvert todo i json using marshall
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error happening", err)
+		return
+	}
+	//convert data in string
+	jsonstring := string(jsonData)
+
+	//convert json data in to reader
+	jsonReader := strings.NewReader(jsonstring)
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos/2"
+
+	//Create put method
+
+	req, err := http.NewRequest(http.MethodPut, myUrl, jsonReader)
+
+	if err != nil {
+		fmt.Println("error in req", err)
+		return
+	}
+
+	req.Header.Set("Content-type", "application/json")
+
+	//Send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("error here in request", err)
+		return
+	}
+	defer res.Body.Close()
+	data, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println("Response during update", string(data), res.StatusCode)
+}
+
+func main() {
+	// readFunction()
+	// createFunction()
+	updateFunction()
 }
